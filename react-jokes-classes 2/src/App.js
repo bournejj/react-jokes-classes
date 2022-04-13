8a7b7b0c1ed085b28355e16a93782df44f146993
bournejj <cras.jbourne@gmail.com>
2022-04-13 16:59:36 +0100

    react jokes creator with classes instead of hooks

---
+++ react-jokes-classes 2/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import JokeList from "./JokeList";
+import JokeListClass from "./JokeListClassComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <JokeListClass />
+    </div>
+  );
+}
+
+export default App;
